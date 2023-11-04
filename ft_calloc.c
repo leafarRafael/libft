cfe842cfb7805c7844388e52b413d6891ce28a70
Rafael <leafarbutzke@hotmail.com>
2023-11-04 14:27:31 -0300

    Enfimm...

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:29:33 by rbutzke           #+#    #+#             */
-/*   Updated: 2023/11/04 12:27:37 by rbutzke          ###   ########.fr       */
+/*   Updated: 2023/11/04 14:23:05 by rbutzke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*array;
 
-	if (size == 0 && nmemb == 0)
-	{
-		size = 1;
-		nmemb = 1;
-	}
 	array = malloc(size * nmemb);
 	if (!array)
 		return (NULL);
